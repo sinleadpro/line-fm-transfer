1a865b52f4695a3aeae4e260c5d7d1f2be9ac55f
PaulPu <paul.pu.cyberbiz@gmail.com>
2023-09-21 15:03:51 +0800

    :sparkles: Add some test html generated by JS

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 function testFunc({ testParams }) {
   console.log({ testParams });
+  // Create the main div element
+  const divElement = document.createElement("div");
+
+  // Create the h1 element and set its content
+  const h1Element = document.createElement("h1");
+  h1Element.textContent = "Hi there!";
+
+  // Create the p element and set its content
+  const pElement = document.createElement("p");
+  pElement.textContent = "Hello, this is my first npm packages.";
+
+  // Append h1 and p elements to the div
+  divElement.appendChild(h1Element);
+  divElement.appendChild(pElement);
+  return divElement;
 }
 
 export default testFunc;
