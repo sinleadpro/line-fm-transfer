1fbaa29fd40bca7dd5824ca138a2210fbbf22da3
PaulPu <paul.pu.cyberbiz@gmail.com>
2023-09-21 15:43:34 +0800

    :bug: Transfer input params real html DOM to string

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 function testFunc({ testParams }) {
-  console.log({ testParams });
-  // Create the main div element
-  const divElement = document.createElement("div");
+  const { title, subTitle, description } = testParams;
+  // console.log({ testParams });
+  // // Create the main div element
+  // const divElement = document.createElement("div");
 
-  // Create the h1 element and set its content
-  const h1Element = document.createElement("h1");
-  h1Element.textContent = "Hi there!";
+  // // Create the h1 element and set its content
+  // const h1Element = document.createElement("h1");
+  // h1Element.textContent = "Hi there!";
 
-  // Create the p element and set its content
-  const pElement = document.createElement("p");
-  pElement.textContent = "Hello, this is my first npm packages.";
+  // // Create the p element and set its content
+  // const pElement = document.createElement("p");
+  // pElement.textContent = "Hello, this is my first npm packages.";
 
-  // Append h1 and p elements to the div
-  divElement.appendChild(h1Element);
-  divElement.appendChild(pElement);
-  console.log({ divElement });
-  return divElement;
+  // // Append h1 and p elements to the div
+  // divElement.appendChild(h1Element);
+  // divElement.appendChild(pElement);
+  // console.log({ divElement });
+  // return divElement;
+  return `<div><h1>${title}</h1><h3>${subTitle}</h3><p>${description}</p></div>`;
 }
 
 export default testFunc;
